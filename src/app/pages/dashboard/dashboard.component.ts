a123d64e66c72a9dd4b5d4305e3eca93b44f4ca1
Aartheeswaran G <roofee425@gmail.com>
2022-08-11 22:13:48 +0530

    fully completed

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router :Router) { }
 
   ngOnInit(): void {
+  }
+  Dashboardpage(){
+    this.router.navigate(['/dashboard'])
+  }
+  Repositorypage(){
+    this.router.navigate(['/repository'])
+  }
+  Monitoringpage(){
+    this.router.navigate(['/monitor'])
+  }
+  Reportspage(){
+    this.router.navigate(['/report'])
   }
 
 }
--- src/app/pages/monitor/monitor.component.ts
+++ src/app/pages/monitor/monitor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-monitor',
   templateUrl: './monitor.component.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonitorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router :Router) { }
 
   ngOnInit(): void {
+  }
+  Dashboardpage(){
+    this.router.navigate(['/dashboard'])
+  }
+  Repositorypage(){
+    this.router.navigate(['/repository'])
+  }
+  Monitoringpage(){
+    this.router.navigate(['/monitor'])
+  }
+  Reportspage(){
+    this.router.navigate(['/report'])
   }
 
 }
